2181ce79dae706997c592b274f5dbf2986967ce8
fstihler <frederik.stihler@berkeley.edu>
2024-11-21 11:10:26 -0800

    Implemented new feedback loop logic in new file

--- autogen_utils.py
+++ autogen_utils.py
@@ -37,3 +37,35 @@ def get_prompt_feedback_code() -> str:
     Output the code as a string, without any coding block or other annotations.
     """
     return prompt
+
+
+def get_prompt_commander() -> str:
+    prompt = """ You are a CAD agent.  Help me render openSCAD code.
+    If you are provided with OpenSCAD code, suggest a tool call to render the OpenSCAD code.
+    """
+    #"If you are provided with OpenSCAD code, suggest a tool call to render the OpenSCAD code, and tell other agents it is in the <img renders/scene.png> file location."
+    return prompt
+
+
+def get_prompt_critics() -> str:
+    prompt = """
+    You are an image feedback agent. 
+    Your role is to examine rendered images (from OpenSCAD code) and confirm whether they match the user’s intended description. 
+    If they do match, respond only with 'TERMINATE_MATCH'.
+    Do not give feedback on the color  and texture.
+    
+    If the image and image description don't match provide suggestions for how the scene can be improved in a structured way.
+    Give your feedback in instructive bullets.
+    Start with 'Feedback:'
+    """
+    return prompt
+
+
+def get_prompt_coder() -> str:
+    prompt = """
+    You are a CAD agent that takes a user request or user feedback and responds only with executable OpenSCAD code. 
+    Please keep the following dimensions in mind when creating the object, as the perspective is fixed for the following space: 100 x 100 x 100
+    You only write CAD code to construct the object, never to render or display it.
+    """
+    #Output the code as a string, without any coding block or other annotations.
+    return prompt
